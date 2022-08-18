f0cede268bfa3c9a94110c2b54f4b1d7586d9eef
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-08-18 09:29:05 +0200

    work on config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,106 +1,109 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const sidebars = require('./sidebars');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const sidebars = require("./sidebars");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Documentation de OP-ENT',
-  tagline: 'l\'ENT open source',
-  url: 'https://docs.op-ent.fr',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "Documentation d'op-ent",
+  tagline: "L'ENT open source",
+  url: "https://docs.op-ent.fr",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'op-ent', // Usually your GitHub org/user name.
-  projectName: 'op-ent', // Usually your repo name.
+  organizationName: "op-ent", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
+    defaultLocale: "fr",
+    locales: ["fr"],
   },
+
+  trailingSlash: true,
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: 'docs',
-          routeBasePath: 'docs',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "docs",
+          routeBasePath: "docs",
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'unstyled-ui',
-        path: 'unstyled-ui',
-        routeBasePath: 'unstyled-ui',
-        sidebarPath: require.resolve('./unstyledUiSidebars')
-      }
+        id: "unstyled-ui",
+        path: "unstyled-ui",
+        routeBasePath: "unstyled-ui",
+        sidebarPath: require.resolve("./unstyledUiSidebars"),
+      },
     ],
   ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'op-ent',
+        title: "op-ent",
         logo: {
-          alt: 'Logo de op-ent.',
-          src: 'img/logo.svg',
+          alt: "Logo de op-ent.",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'home',
-            position: 'left',
-            label: 'Documentation d\'op-ent',
+            type: "doc",
+            docId: "home",
+            position: "left",
+            label: "Documentation d'op-ent",
           },
           {
-            href: 'https://github.com/op-ent',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/op-ent",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Home',
-                to: '/docs/home',
+                label: "Home",
+                to: "/docs/home",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discussion Github',
-                href: 'https://github.com/op-ent/op-ent',
+                label: "Discussion Github",
+                href: "https://github.com/op-ent/op-ent",
               },
               {
-                label: 'Discord',
-                href: 'https://discord.gg/kMFmfSbC2C',
+                label: "Discord",
+                href: "https://discord.gg/kMFmfSbC2C",
               },
             ],
           },
