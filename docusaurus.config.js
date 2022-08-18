46d9da5cc928215858b823e58beb35e386f36b84
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-08-18 18:02:37 +0200

    update footer links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,29 +76,58 @@ module.exports = {
       footer: {
         links: [
           {
-            title: "Docs",
+            title: "Projet",
             items: [
               {
-                label: "Home",
-                to: "/docs/home",
+                label: "Blog",
+                href: "https://op-ent.fr/blog",
+              },
+              {
+                label: "Packages",
+                to: "/packages",
               },
             ],
           },
           {
-            title: "Community",
+            title: "Ressources",
             items: [
               {
-                label: "Discussion Github",
-                href: "https://github.com/op-ent/op-ent",
+                label: "Identité graphique",
+                href: "https://github.com/op-ent/brand",
+              },
+              {
+                label: "Documentation",
+                to: "/",
+              },
+              {
+                label: "Support",
+                href: "https://github.com/op-ent/op-ent/issues",
+              },
+              {
+                label: "Etat des systèmes",
+                href: "https://status.op-ent.fr",
+              },
+            ],
+          },
+          {
+            title: "Communauté",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/op-ent",
               },
               {
                 label: "Discord",
                 href: "https://discord.gg/kMFmfSbC2C",
               },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/opent_oss",
+              },
             ],
           },
         ],
-        copyright: `&copy; ${new Date().getFullYear()} op-ent.`,
+        copyright: `Copyright &copy; ${new Date().getFullYear()} op-ent.`,
       },
       prism: {
         theme: lightCodeTheme,
