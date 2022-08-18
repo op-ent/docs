03736f21067296b92cb632a8f2735c6e81b69d9d
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-08-18 18:03:51 +0200

    make color mode toggle browser only

--- src/theme/ColorModeToggle.tsx
+++ src/theme/ColorModeToggle.tsx
@@ -5,6 +5,7 @@ import { translate } from "@docusaurus/Translate";
 import { Props } from "@theme/ColorModeToggle";
 import { Switch } from "@headlessui/react";
 import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 function ColorModeToggle({ value, onChange }: Props) {
   const isBrowser = useIsBrowser();
   const title = translate(
@@ -32,47 +33,51 @@ function ColorModeToggle({ value, onChange }: Props) {
   const enabled = value === "dark";
   const setEnabled = (checked: boolean) => onChange(checked ? "dark" : "light");
   return (
-    <Switch
-      checked={enabled}
-      onChange={setEnabled}
-      className={clsx(
-        enabled ? "bg-primary" : "bg-gray-300",
-        "p-0 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:cursor-not-allowed dark:ring-offset-[color:var(--ifm-navbar-background-color)]"
+    <BrowserOnly>
+      {() => (
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className={clsx(
+            enabled ? "bg-primary" : "bg-gray-300",
+            "p-0 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:cursor-not-allowed dark:ring-offset-[color:var(--ifm-navbar-background-color)]"
+          )}
+          disabled={!isBrowser}
+          title={title}
+          aria-label={title}
+        >
+          <span
+            className={clsx(
+              enabled ? "translate-x-5" : "translate-x-0",
+              "pointer-events-none relative inline-block h-5 w-5 rounded-full bg-[color:var(--ifm-navbar-background-color)] shadow transform ring-0 transition ease-in-out duration-200"
+            )}
+          >
+            <span
+              className={clsx(
+                enabled
+                  ? "opacity-0 ease-out duration-100"
+                  : "opacity-100 ease-in duration-200",
+                "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+              )}
+              aria-hidden="true"
+            >
+              <SunIcon className="h-3 w-3 text-gray-500" />
+            </span>
+            <span
+              className={clsx(
+                enabled
+                  ? "opacity-100 ease-in duration-200"
+                  : "opacity-0 ease-out duration-100",
+                "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+              )}
+              aria-hidden="true"
+            >
+              <MoonIcon className="h-3 w-3 text-primary" />
+            </span>
+          </span>
+        </Switch>
       )}
-      disabled={!isBrowser}
-      title={title}
-      aria-label={title}
-    >
-      <span
-        className={clsx(
-          enabled ? "translate-x-5" : "translate-x-0",
-          "pointer-events-none relative inline-block h-5 w-5 rounded-full bg-[color:var(--ifm-navbar-background-color)] shadow transform ring-0 transition ease-in-out duration-200"
-        )}
-      >
-        <span
-          className={clsx(
-            enabled
-              ? "opacity-0 ease-out duration-100"
-              : "opacity-100 ease-in duration-200",
-            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-          )}
-          aria-hidden="true"
-        >
-          <SunIcon className="h-3 w-3 text-gray-500" />
-        </span>
-        <span
-          className={clsx(
-            enabled
-              ? "opacity-100 ease-in duration-200"
-              : "opacity-0 ease-out duration-100",
-            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-          )}
-          aria-hidden="true"
-        >
-          <MoonIcon className="h-3 w-3 text-primary" />
-        </span>
-      </span>
-    </Switch>
+    </BrowserOnly>
   );
 }
 export default React.memo(ColorModeToggle);
